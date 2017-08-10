d5f445cd0e33f9bf6e85f5063a527f6ae98c1658
Alexx Perloff <aperloff@physics.tamu.edu>
2017-08-10 02:54:59 -0500

    Make a working version of JetCorrectionUncertainty containing JetCorrectionUncertaintyCollection.

--- MetUncertainty/interface/JetCorrectionUncertainty.hh
+++ MetUncertainty/interface/JetCorrectionUncertainty.hh
@@ -4,8 +4,8 @@
 //
 // Generic parameters for Jet corrections
 //
-#ifndef JetUncertainty_h
-#define JetUncertainty_h
+#ifndef JetCorrectionUncertainty_h
+#define JetCorrectionUncertainty_h
 
 #include "CondFormats/Serialization/interface/Serializable.h"
 #include "CondFormats/JetMETObjects/interface/Utilities.h"
@@ -25,8 +25,8 @@
 std::ostream& operator<<(std::ostream& out, const JetCorrectorParameters::Record& fBin);
 
 
-class JetUncertaintyCollection {
-  //---------------- JetUncertaintyCollection class ----------------
+class JetCorrectionUncertaintyCollection {
+  //---------------- JetCorrectionUncertaintyCollection class ----------------
 
  public:
   enum Level_t {
@@ -86,16 +86,20 @@ class JetUncertaintyCollection {
      CorrelationGroupUncorrelated=53
   };
 
-  enum L5_Species_t {L5_bJ=0,L5_cJ,L5_qJ,L5_gJ,L5_bT,L5_cT,L5_qT,L5_gT,N_L5_SPECIES};
-  enum L7_Species_t {L7_gJ=0,L7_qJ,L7_cJ,L7_bJ,L7_jJ,L7_qT,L7_cT,L7_bT,L7_jT,N_L7_SPECIES};
   typedef int                            key_type;
   typedef std::string                    label_type;
   typedef JetCorrectorParameters         value_type;
   typedef std::pair<key_type,value_type> pair_type;
   typedef std::vector<pair_type>         collection_type;
 
+  // Default constructor
+  JetCorrectionUncertaintyCollection() { corrections_.clear(); }
+
+  // Constructor
+  JetCorrectorParametersCollection( std::vector<label_type> chosen_sections );
+
   // Add a JetCorrectorParameter object, possibly with flavor.
-  void push_back( key_type i, value_type const & j, label_type const & flav = "" );
+  void push_back( key_type i, value_type const & j);
 
   // Access the JetCorrectorParameter via the key k.
   // key_type is hashed to deal with the three collections
@@ -117,30 +121,19 @@ class JetUncertaintyCollection {
   static void getSections(std::string inputFile,
                           std::vector<std::string> & outputs );
 
-  // Find the L5 bin for hashing
-  static key_type getL5Bin( std::string const & flav );
-  // Find the L7 bin for hashing
-  static key_type getL7Bin( std::string const & flav );
-  // Check if this is an L5 hashed value
-  static bool isL5( key_type k );
-  // Check if this is an L7 hashed value
-  static bool isL7( key_type k );
-
+  // Find the label corresponding to each key
   static std::string findLabel( key_type k );
-  static std::string findL5Flavor( key_type k );
-  static std::string findL7Parton( key_type k );
 
+  const collection_type& getUncertainties() const  {return uncertainties_;}
 
-  collection_type                        corrections_;
-  collection_type&                       getCorrections()   {return corrections_;}
-
+  float getTotalUncertainty();
 
  protected:
 
   // Find the key corresponding to each label
   key_type findKey( std::string const & label ) const;
 
-
+  collection_type uncertainties_;
 
  COND_SERIALIZABLE;
 
